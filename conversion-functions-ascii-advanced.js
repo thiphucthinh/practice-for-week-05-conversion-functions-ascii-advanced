cea0d7aa4984242a8dd4ff294d3584ef827d1716
Thi Phuc Thinh Huynh <ph.thinh905@gmail.com>
2023-09-21 20:32:05 -0700

    All solutions

--- conversion-functions-ascii-advanced.js
+++ conversion-functions-ascii-advanced.js
@@ -9,62 +9,71 @@ Consult documentation on how to utilize the following methods:
 
 /* Base 2 to base 16 */
 function binaryToHexadecimal(blob) {
-  // Your code here
+  // Base 2 to base 10
+  let newInput = blob.split("").slice(2).join("");
+  let mid = parseInt(newInput, 2);
+  // Base 10 to base 16
+  return "0x" + mid.toString(16);
 }
 
 /* Base 16 to base 2 */
 function hexadecimalToBinary(blob) {
-  // Your code here
+  // Base 16 to base 10
+  let mid = parseInt(blob);
+  // Base 10 to base 2
+  return "0b" + mid.toString(2);
 }
 
 /* Base 10 to ASCII */
 function decimalToAscii(blob) {
-  // Your code here
+  return String.fromCharCode(blob);
 }
 
 /* Base 2 to ASCII */
 function binaryToAscii(blob) {
-  // Your code here
+  return String.fromCharCode(blob);
 }
 
 /* Base 16 to ASCII */
 function hexadecimalToAscii(blob) {
-  // Your code here
+  return String.fromCharCode(blob);
 }
 
 /* ASCII to base 10 */
 function asciiToDecimal(blob) {
-  // Your code here
+  return blob.split("").map(el => el.charCodeAt(0));
 }
 
-// console.log('Binary to hexadecimal:')
-// console.log(binaryToHexadecimal('0b1010')) // '0xa'
-// console.log(binaryToHexadecimal('0b0011')) // '0x3'
-// console.log('')
 
-// console.log('Hexadecimal to binary:')
-// console.log(hexadecimalToBinary('0xa1'))  // '0b10100001'
-// console.log(hexadecimalToBinary('0xff'))  // '0b11111111'
-// console.log('')
+// Testing
+console.log('Binary to hexadecimal:')
+console.log(binaryToHexadecimal('0b1010')) // '0xa'
+console.log(binaryToHexadecimal('0b0011')) // '0x3'
+console.log('')
 
-// console.log('Decimal to ASCII:')
-// console.log(decimalToAscii(65))  // 'A'
-// console.log(decimalToAscii(97))  // 'a'
-// console.log('')
+console.log('Hexadecimal to binary:')
+console.log(hexadecimalToBinary('0xa1'))  // '0b10100001'
+console.log(hexadecimalToBinary('0xff'))  // '0b11111111'
+console.log('')
 
-// console.log('Binary to ASCII:')
-// console.log(binaryToAscii('0b01000010'))  // 'B'
-// console.log(binaryToAscii('0b01100010'))  // 'b'
-// console.log('')
+console.log('Decimal to ASCII:')
+console.log(decimalToAscii(65))  // 'A'
+console.log(decimalToAscii(97))  // 'a'
+console.log('')
 
-// console.log('Hexadecimal to ASCII:')
-// console.log(hexadecimalToAscii('0x43'))  // 'C'
-// console.log(hexadecimalToAscii('0x63'))  // 'c'
-// console.log('')
+console.log('Binary to ASCII:')
+console.log(binaryToAscii('0b01000010'))  // 'B'
+console.log(binaryToAscii('0b01100010'))  // 'b'
+console.log('')
 
-// console.log('ASCII to decimal:')
-// console.log(asciiToDecimal('HELLO'))   // [ 72, 69, 76, 76, 79 ]
-// console.log(asciiToDecimal('world!'))  // [ 119, 111, 114, 108, 100, 33 ]
+console.log('Hexadecimal to ASCII:')
+console.log(hexadecimalToAscii('0x43'))  // 'C'
+console.log(hexadecimalToAscii('0x63'))  // 'c'
+console.log('')
+
+console.log('ASCII to decimal:')
+console.log(asciiToDecimal('HELLO'))   // [ 72, 69, 76, 76, 79 ]
+console.log(asciiToDecimal('world!'))  // [ 119, 111, 114, 108, 100, 33 ]
 
 module.exports = {
   binaryToHexadecimal,
